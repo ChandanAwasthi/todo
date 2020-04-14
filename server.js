d500f21253b07a0e07117102cc68c72342764250
Chandan Awasthi <chandan.awasthi@nagarro.com>
2020-04-14 23:39:04 +0530

    Updated to check is isNaN

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const express =  require('express');
 const app = express();
 
-app.listen(6543, () => console.log('listening at 3000'));
-app.use(express.static('public'));
+let todos = [
+    {title: 'Learn Node JS', description: 'Quite a good', dueDate: '2020-04-05', priority: 'medium', notes = ['This is first note', 'Second note']}
+]
+//, status: 'Incomplete'
+app.get('/todos', (request, response) => {
+    response.send(todos);
+});
+
+app.get('/todos/:id', (request,response) => {
+    if(isNaN(parseInt(request.params.id))){
+        console.log('Invalid');
+        response.status(404).send({
+            error: 'Invalid todo id'
+     })
+     return
+    };
+    response.send('');
+
+});
+app.listen(5432, () => console.log('listening at 5432'));
+//app.use(express.static('public'));
+
